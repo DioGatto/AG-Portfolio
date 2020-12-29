72ac24a854e6f3938e62ea754c7141c8d5073e55
Andre <andreagrieco01@gmail.com>
2020-12-29 21:42:36 +0100

    from html to php + bug fixes

--- index.php
+++ index.php
@@ -1,50 +1,8 @@
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/style-nav.css">
-    <link rel="stylesheet" href="css/style-page.css">
-    <link rel="stylesheet" href="css/style-footer.css">
-    <link rel="stylesheet" href="css/style-form.css">
-
-    <title>AG Portfolio</title>
-    <script>
-        alert('Attenzione, la pagina è W.I.P');
-    </script>
-</head>
+<?php include('page/heading.php'); ?>
 <body>
-    <div class="navbar">
-        <div class="inner-nav">
-            <div class="section" id="me">
-                <img src="img/me.jpg" alt="le me" width="100px">
-                <span id="name">Andrea Grieco</span>
-                <span id="lavoro">Sviluppatore</span>
-            </div>
-            <div class="section">
-                <a href="portfolio.html">Portfolio</a>
-                <a href="curriculum.html">Curriculum</a>
-            </div>
-            <div class="section">
-                <a href="curriculum.html#me">chi sono ?</a>
-                <a href="curriculum.html#studies">i miei studi</a>
-            </div>
-            <div class="section">
-                <a href="#contactme">contattami</a>
-            </div>
-            <div class="section" id="social">
-                <a href="https://www.linkedin.com/in/andrea-grieco-2001/"><img src="img/icons/linkedin-brands.svg" alt="Linkedin"></a>
-                <a href="mailto:andreagrieco01@gmail.com"><img src="img/icons/envelope-regular.svg" alt="Gmail"></a>
-                <a href="https://github.com/DioGatto"><img src="img/icons/github-brands.svg" alt="GitHub"></a>
-                <a href="https://t.me/DioGatto2001"><img src="img/icons/telegram-plane-brands.svg" alt="Telegram"></a>
-            </div>
-        </div>
-    </div>
+<?php include('page/navbar.php'); ?>
     <div class="page">
         <div class="content">
             <div class="box rounded">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam sequi quaerat architecto vel eum iure ad, ea doloremque temporibus iusto, quae rerum, accusantium dolorum et error. Delectus alias repudiandae id?</div>
