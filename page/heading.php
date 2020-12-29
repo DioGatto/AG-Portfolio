7d351f344cb5e8c0bee836b8ec507870df1e562b
Andre <andreagrieco01@gmail.com>
2020-12-29 21:42:21 +0100

    from html to php + bug fixes

---
+++ page/heading.php
@@ -0,0 +1,22 @@
+<?php
+    echo('
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+            <link rel="stylesheet" href="css/style.css">
+            <link rel="stylesheet" href="css/style-nav.css">
+            <link rel="stylesheet" href="css/style-page.css">
+            <link rel="stylesheet" href="css/style-footer.css">
+            <link rel="stylesheet" href="css/style-form.css">
+
+            <title>AG Portfolio</title>
+            <script>
+                alert("Attenzione, la pagina è W.I.P");
+            </script>
+        </head>
+    ');
+?>
