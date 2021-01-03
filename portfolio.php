8739aae9a26ee773603ccf782c450f072a6595c4
Andre <andreagrieco01@gmail.com>
2021-01-03 18:44:25 +0100

    include xml

--- portfolio.php
+++ portfolio.php
@@ -6,39 +6,27 @@
     <div class="page">
         <div class="content">
             <div class="title">Qui puoi vedere tutti i miei lavori</div>
-            <div class="box">
-                <div class="title">AG Portfolio</div>
-                <div class="inner-box">
-                    <div class="left">
-                        <a href="index.html"><img src="img/me.jpg"></a>
-                    </div>
-                    <div class="right">
-                        <div class="description">Il portfolio che stai visualizzando</div>
-                    </div>
-                </div>
-            </div>
-            <div class="box">
-                <div class="title">AG Portfolio</div>
-                <div class="inner-box">
-                    <div class="left">
-                        <a href="index.html"><img src="img/me.jpg"></a>
-                    </div>
-                    <div class="right">
-                        <div class="description">Il portfolio che stai visualizzando</div>
-                    </div>
-                </div>
-            </div>
-            <div class="box">
-                <div class="title">AG Portfolio</div>
-                <div class="inner-box">
-                    <div class="left">
-                        <a href="index.html"><img src="img/me.jpg"></a>
-                    </div>
-                    <div class="right">
-                        <div class="description">Il portfolio che stai visualizzando</div>
-                    </div>
-                </div>
-            </div>
+
+            <?php
+                $file = file_get_contents('database/jobs.json');
+                $json = json_decode($file);
+                foreach ($json as $key => $value) {
+                    echo('
+                        <div class="box">
+                        <div class="title">'.$value->titolo.'</div>
+                            <div class="inner-box">
+                                <div class="left">
+                                    <a href="'.$value->link.'"><img src="'.$value->immagine.'"></a>
+                                </div>
+                                <div class="right">
+                                    <div class="description">'.$value->descrizione.'</div>
+                                </div>
+                            </div>
+                        </div>
+                    ');
+                    
+                }
+            ?>
         </div>
         <?php include('contents/form.html'); ?>
         <?php include('contents/footer.html'); ?>
